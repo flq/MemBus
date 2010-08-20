1a8863421d775ce596a4358d0f9a3560c7bff88b
flq <fquednau@gmail.com>
2010-08-20 16:50:08 +0200

    Testing integration with reactive framework

---
+++ Membus.Reactive.Tests/Help/MessageA.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Membus.Reactive.Tests.Help
+{
+    public class MessageA
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Membus.Reactive.Tests/When_MemBus_is_used_as_Observable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MemBus;
+using MemBus.Configurators;
+using Membus.Reactive.Tests.Help;
+using NUnit.Framework;
+using MemBus.Tests.Frame;
+
+namespace Membus.Reactive.Tests
+{
+    [TestFixture]
+    public class When_MemBus_is_used_as_Observable
+    {
+        [Test]
+        public void Apply_extension_method_where()
+        {
+            int msgCount = 0;
+            var mb = BusSetup.StartWith<Conservative>().Construct();
+            var msgs = 
+                from msg in mb.Observe<MessageA>() 
+                where msg.Name == "A" 
+                select msg;
+
+            using (msgs.Subscribe(msg => msgCount++))
+            {
+                mb.Publish(new MessageA {Name = "A"});
+                mb.Publish(new MessageA {Name = "B"});
+                mb.Publish(new MessageA {Name = "A"});
+                msgCount.ShouldBeEqualTo(2);
+            }
+        }
+    }
+}
