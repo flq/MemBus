e2fa443f852ae528f2557167be97c1df9d180c8a
flq <fquednau@gmail.com>
2010-12-13 22:08:28 +0100

    Made revision variable to play nice with updates

--- MemBus/Properties/AssemblyInfo.cs
+++ MemBus/Properties/AssemblyInfo.cs
@@ -3,27 +3,16 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("MemBus")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Publish Subscribe in-memory")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Frank Quednau")]
 [assembly: AssemblyProduct("MemBus")]
-[assembly: AssemblyCopyright("Apache 2.0 licensed")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Apache 2.0 licensed 2010 realfiction (F Quednau)")]
+
 
 [assembly: ComVisible(false)]
 
 [assembly:InternalsVisibleTo("MemBus.Tests")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.*")]
