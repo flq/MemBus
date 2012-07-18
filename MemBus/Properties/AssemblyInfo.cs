f30c2fed80a11df385cd34d1f3b2d2e652c91386
flq <fquednau@gmail.com>
2012-07-18 19:37:19 +0200

    Version, nuspec update

--- MemBus/Properties/AssemblyInfo.cs
+++ MemBus/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly:InternalsVisibleTo("MemBus.Tests")]
 
-[assembly: AssemblyVersion("1.5.3")]
-[assembly: AssemblyFileVersion("1.5.3")]
+[assembly: AssemblyVersion("1.5.4")]
+[assembly: AssemblyFileVersion("1.5.4")]
